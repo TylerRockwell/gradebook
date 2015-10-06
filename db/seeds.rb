dc9c7a00cca9b530267f27a0be9bb31015008dad
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-10-06 14:33:46 -0400

    Add database associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+poak = Teacher.create!(name: "Professor Oak", email: "poak@example.com")
+poak.password = "hunter2"
+poak.save
